8acb2ab7763f7e72f80c0534ad1c02824e0d00a0
lkj <jklj>
2016-08-04 09:49:48 -0400

    create project

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function circle(x, y, diameter) {
+  ellipse(x,y,diameter,diameter);
+}
+
+function draw() {
+  fill(255,255,51);
+  ellipse(50, 50, 60, 60);
+  line(400,200,400,55);
+    
+} 
+function setup() {
+  createCanvas(640,480);
+}
+
+
